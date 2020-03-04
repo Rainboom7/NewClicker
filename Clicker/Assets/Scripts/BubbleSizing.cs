e1727e0fd51b2b5b4750aa3a7ae04c86a372a941
Rainboom7 <53229475+Rainboom7@users.noreply.github.com>
2020-03-04 22:21:50 +0300

    Added expanding bubble and background

---
+++ Clicker/Assets/Scripts/BubbleSizing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSizing : MonoBehaviour
+{
+    private Vector3 currentScale;
+    public List<ActionBehaviour> actions;
+    private void Rescale()
+    {
+
+        transform.localScale += currentScale * 0.01f;
+    }
+    private void OnEnable()
+    {
+        currentScale = transform.localScale;
+    }
+    private void FixedUpdate()
+    {
+        Rescale();
+        if (transform.localScale.x >= currentScale.x * 1.5f) {
+            foreach (var action in actions)
+            {
+                action.Execute();
+
+            }
+        }
+    }
+}
---
+++ Clicker/Assets/Scripts/DestroyAction.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAction : ActionBehaviour
+{
+ public override void Execute() {
+        Destroy(gameObject);
+    }
+}
